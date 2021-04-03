f5b16efbad579ec4f196715d510a65db5e38d330
Abuu001 <abulugz@gmail.com>
2021-04-04 00:38:01 +0300

    Added proceed to payments

--- client/src/components/Checkout/Subtotal/Subtotal.js
+++ client/src/components/Checkout/Subtotal/Subtotal.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import CurrencyFormat from 'react-currency-format';
+import { useHistory } from 'react-router';
 import { getBasketTotal } from '../../ContextAPI/reducer';
 import { useStateValue } from '../../ContextAPI/StateProvider';
 import "./Subtotal.css"
 
 function Subtotal() {
     
+    const history = useHistory();
     const [{basket},dispatch] = useStateValue();
     return (
          <div className="Subtotal">
@@ -24,7 +26,7 @@ function Subtotal() {
                 thousandSeparator={true}
                 prefix={"$"}  
             />
-            <button>Proceed to Checkout</button>
+            <button onClick={e=>history.push('/payment')}>Proceed to Checkout</button>
         </div>
    
     )

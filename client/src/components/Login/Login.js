75f24441689ae43e8c4d2b9059b0d1e363386373
Abuu001 <abulugz@gmail.com>
2021-04-03 21:54:27 +0300

    Added login form

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,19 +1,43 @@
-import { Link } from 'react-router-dom'
+import { Link,useHistory } from 'react-router-dom'
 import React, { useState } from 'react'
 import "./Login.css"
+import { auth , provider } from '../../firebase';
+import { CircularProgress } from '@material-ui/core';
 
 function Login() {
 
+    const history =useHistory()
     const [email,setEmail] =useState('');
     const [password,setPassword] =useState('');
+    const [spinner,setSpinner] =useState(false);
 
-    const signIn=(e)=>{
+    const signIn=async (e)=>{
         e.preventDefault();
+
+        if(email !=="" && password !==""){
+            setSpinner(true)
+        }
+       await auth.signInWithEmailAndPassword(email,password)
+        .then(auth=>{
+     
+            history.push("/")
+        }).catch(error=>alert(error.message))
 
     }
 
-    const register=(e)=>{
+    const signWithGoogle=async (e)=>{
         e.preventDefault();
+
+        await auth.signInWithPopup(provider)
+        .then(result=>{
+
+            if(result.additionalUserInfo.profile.email &&  result.additionalUserInfo.profile.verified_email){
+                history.push('/')
+            }else{
+                history.push('/login')
+            }
+
+        }).catch(error=>alert(error.message))
     }
 
     return (
@@ -23,8 +47,9 @@ function Login() {
             </Link>
             <h4 id="login__siteName">Baked By Berry</h4>
             <div className="login__container">
-                <h1>Sign In</h1>
-                <form>
+                <span id="spinner"><h1>Sign In</h1>  {spinner ?  <CircularProgress  color="secondary"fontSize="smaller"/> : null} </span>
+                
+                <form> 
                     <h5>E mail </h5> 
                     <input type="email" required value={email }  onChange={e=>setEmail(e.target.value)}/>
 
@@ -34,6 +59,10 @@ function Login() {
                     <button  className="login__signInBtn" onClick={signIn}>Sign In</button>
                 </form>
 
+                <b id="orwithGoogle">or</b>
+                <span className="GoogleIcon">
+                    Sign In with   <svg width="24" height="25"onClick={signWithGoogle}  viewBox="0 0 256 262" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid"><path d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622 38.755 30.023 2.685.268c24.659-22.774 38.875-56.282 38.875-96.027" fill="#4285F4"/><path d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055-34.523 0-63.824-22.773-74.269-54.25l-1.531.13-40.298 31.187-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1" fill="#34A853"/><path d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82 0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602l42.356-32.782" fill="#FBBC05"/><path d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0 79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251" fill="#EB4335"/></svg>
+                </span>
                 <p>
                     By signing-in you agreee to the Baked By Berry
                     Conditions of use & Sale.Please
@@ -41,7 +70,9 @@ function Login() {
                     and our Interset Based Ads.  <strong>Notice</strong>
                 </p>
 
-                 <button className="login__registerBtn" onClick={register}>Create your Baked By Berry Account</button>
+                <Link to="/register">
+                    <button className="login__registerBtn"  >Create your Baked By Berry Account</button>
+                </Link>
             </div>
        </div>
     )
